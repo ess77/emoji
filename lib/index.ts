5771f325c565d966bd6c820e4afbcd90f1597d70
ESS Systemes & Services <lyrbat@gmail.com>
2020-02-21 17:34:58 +0100

    commit du master

--- lib/index.ts
+++ lib/index.ts
@@ -3,6 +3,7 @@ import { toShort } from 'emojione';
 const EMOJI_SHORTCODES = /:[a-zA-Z1-9_]+:/g;
 
 export function findEmojis(str: string): string[] {
+    
     if(typeof str != 'string') {
         return [];
     }
